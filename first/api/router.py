dd381cb060b308a81dbd26f67564df14a2892236
denis-nahuamel <nahuamel.denis@gmail.com>
2022-09-21 02:07:11 +0000

    post api works

--- first/api/router.py
+++ first/api/router.py
@@ -2,4 +2,4 @@ from rest_framework.routers import DefaultRouter
 from first.api.views import PostApiViewSet
 
 router_post = DefaultRouter()
-router_post.register(prefix='post', basename='post', viewsets=PostApiViewSet)
+router_post.register(prefix='post', basename='post', viewset=PostApiViewSet)
